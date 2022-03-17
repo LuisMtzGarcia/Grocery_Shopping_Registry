638e6085ba9fa0e4f8e209463796597e3af08447
Luis <ing.luismtzgarcia@gmail.com>
2022-03-17 15:38:32 -0600

    Deploying to use test account in heroku site.

--- shopping_registry/views.py
+++ shopping_registry/views.py
@@ -22,7 +22,7 @@ def check_account(username, object):
     """Checks if the user is using the demo account and if they're the owner of 
         the object."""
     # Demo account check.
-    if username == 'supercuenta':
+    if username == '2supercuenta':
         raise PermissionDenied
 
     # Owner of the object check.
